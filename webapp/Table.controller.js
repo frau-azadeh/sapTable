51f37968c2e0405d1c1d29cdf9d46a1148c15a3d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-13 04:33:45 -0800

    add table.controller.js

--- webapp/Table.controller.js
+++ webapp/Table.controller.js
@@ -1,22 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-  ], function (Controller, JSONModel) {
-    "use strict";
-  
-    return Controller.extend("sap.ui.demo.Table.controller.Table", {
-  
-      onInit: function () {
-        var oModel = new JSONModel({
-          Products: [
-            { ProductName: "Apple", Price: "1.00" },
-            { ProductName: "Orange", Price: "0.80" },
-            { ProductName: "Banana", Price: "0.50" }
-          ]
-        });
-        this.getView().setModel(oModel);
-      }
-  
-    });
-  });
-  
+  'sap/ui/core/mvc/Controller',
+  'sap/ui/model/json/JSONModel'
+], function(Controller, JSONModel) {
+"use strict";
+
+var TableController = Controller.extend("sap.m.sample.TableNavigated.Table", {
+
+  onInit: function () {
+    var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+    var oSettingsModel = new JSONModel({ navigatedItem: ""});
+    this.getView().setModel(oModel);
+    this.getView().setModel(oSettingsModel, 'settings');
+  },
+
+  onPress: function (oEvent) {
+    var oItem = oEvent.getSource();
+    var oBindingContext = oItem.getBindingContext();
+    var oModel = this.getView().getModel();
+    var oSettingsModel = this.getView().getModel('settings');
+    oSettingsModel.setProperty("/navigatedItem", oModel.getProperty("ProductId", oBindingContext));
+  },
+
+  isNavigated: function(sNavigatedItemId, sItemId) {
+    return sNavigatedItemId === sItemId;
+  }
+});
+
+
+return TableController;
+
+});
